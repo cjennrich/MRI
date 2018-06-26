517b4088a2467b6beff94209f4e7a3a46c262cef
Chris Jennrich <jennr013@d.umn.edu>
2018-06-26 16:52:10 -0500

    Basic menu buttons added, new activities needed for each.

---
+++ app/src/main/java/jennrich_mri/mri/MainActivity.java
@@ -0,0 +1,13 @@
+package jennrich_mri.mri;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
